9b3888b74d52ed49baaef2ceefbeca747aa78964
Bouchra Akl <bushraakl1234@gmail.com>
2025-02-13 15:03:18 -0300

    feat: enhance PortalButton component with disabled state and update lesson fetching logic

--- src/components/PortalButton.js
+++ src/components/PortalButton.js
@@ -2,9 +2,13 @@ import React from "react";
 import "bootstrap-icons/font/bootstrap-icons.css";
 import "../styles/PortalButton.css";
 
-const PortalButton = ({ icon, label, onClick }) => {
+const PortalButton = ({ icon, label, onClick, disabled }) => {
   return (
-    <button className="portal-btn" onClick={onClick}>
+    <button 
+      className={`portal-btn ${disabled ? 'disabled' : ''}`} 
+      onClick={onClick} 
+      disabled={disabled}
+    >
       {icon && (
         <div className="portal-icon">
           <i className={`bi ${icon}`}></i>
